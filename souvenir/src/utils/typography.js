42da26489ac4b73994275965271d2964e2b35ee8
dev <dev@devs-Mac-Pro.local>
2020-05-31 04:04:41 +0800

    add

--- souvenir/src/utils/typography.js
+++ souvenir/src/utils/typography.js
@@ -1,5 +1,6 @@
 import Typography from "typography"
 import Wordpress2016 from "typography-theme-wordpress-2016"
+import '../css/global.css';
 
 Wordpress2016.overrideThemeStyles = () => {
   return {
